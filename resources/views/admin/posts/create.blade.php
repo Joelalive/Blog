3854aa8f66693fdda20929c27c49b3c2ebb577f1
“joelalive” <joelimalive1994@gmail.com>
2019-03-02 13:27:44 +0530

    Till Episode 25

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -2,6 +2,33 @@
 
 @section('content')
 
-<h2 class="text-center">Create New Post</h2>
+<div class="card card-default">
+<div class="card-header">
+Create New Post
+</div>
+<div class="card-body">
+<form action="{{route('post.store')}}" method="post">
+@csrf
+<div class="form-group">
+<label for="title">Title</label>
+<input type="text" name="title" id="" class="form-control">
+</div>
+<div class="form-group">
+<label for="featured">Featured Image</label>
+<input type="file" name="featured" id="" class="form-control">
+</div>
+<div class="form-group">
+<label for="content">Content</label>
+<textarea name="content" id="" cols="5" rows="5" class="form-control"></textarea>
+</div>
+<div class="form-group">
+    <div class="text-center">
+    <button type="submit" class="btn btn-success">Store Post</button>
+    </div>
+</div>
+</form>
+</div>
+</div>
+
 
 @endsection
